998f6e08a2560301087506879f91ffec69b8dd43
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-18 20:27:09 +0600

    61-4 recap verify token

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const verifyToken = (req, res, next) => {
   if (!token) {
     return res.status(401).send({ message: "unAuthorized access" });
   }
+  // verify the token
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).send({ message: "unAuthorized access" });
