3e3cfdf54a1a0f71d67e1d670266b7459427d1c4
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-18 13:15:13 +0600

    60-3 Use Cookie Parser And Set Token To Httponly Cookie

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4nvaj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -36,12 +38,17 @@ async function run() {
       .db("jobPortal")
       .collection("job_applications");
 
-      // Auth related APIs
-      app.post('/jwt',(req,res)=>{
-        const user = req.body;
-        const token = jwt.sign(user, 'secret', {expiresIn: '1h'});
-        res.send(token);
-      })
+    // Auth related APIs
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false, // http://localhost:5173/signIn
+        })
+        .send({ sucess: true });
+    });
 
     // get all job
     app.get("/jobs", async (req, res) => {
